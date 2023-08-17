51e14293edc1c797b63236959ff8de029c9f8fb3
vkuehn <vkuehn@more-machines.org>
2023-08-17 10:08:11 +0000

    be curious

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "automerge": false,
+    "docker-compose": {"enabled": true},
+    "dryRun" : "true",
+    "extends": ["config:recommended", ":skipStatusChecks"],
+    "gitLabIgnoreApprovals": false,
+    "platformAutomerge": false,
+    "platform": "local"
+  }
