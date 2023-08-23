260842af4d06a1b078c7746ef6f022a7c1d1e97e
vkuehn <vkuehn@more-machines.org>
2023-08-23 12:34:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "automerge": false,
-    "autodiscover": false,
-    "debug": true,
-    "autodiscoverFilter": ["renovate-try/*"],
-    "docker-compose": {"enabled": true},
-    "dryRun" : "true",
-    "extends": ["config:recommended", ":skipStatusChecks"],
-    "gitLabIgnoreApprovals": false,
-    "onboarding": false,
-    "platformAutomerge": false,
-    "platform": "github",  
-    "requireConfig": false
-  }
+     "automerge": false,
+     "autodiscover": false,
+     "debug": true,
+     "autodiscoverFilter": ["renovate-try/*"],
+     "docker-compose": {"enabled": true},
+     "dryRun": "true",
+     "extends": ["config:recommended", ":skipStatusChecks"],
+     "gitLabIgnoreApprovals": false,
+     "onboarding": false,
+     "platformAutomerge": false,
+     "platform": "github",
+     "requireConfig": "optional"
+}
